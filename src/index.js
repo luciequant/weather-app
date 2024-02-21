fae21afaaeff562deca257c9b959768620e4991a
luciequant <156855912+luciequant@users.noreply.github.com>
2024-02-21 17:49:31 +0100

    Added API

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
-function searchCity(event) {
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+}
+function searchCity(city) {
+  let apiKey = "5fb1305ca3a3b7a0bo4908f8ftf4371a";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
+function enterCity(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("submit", searchCity);
+searchFormElement.addEventListener("submit", enterCity);
+
+searchCity("Madrid");
